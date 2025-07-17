cf5a1eaa9a36914459e7c5899b27599d8a3e38e8
Collier-A-B <benjaminac46@gmail.com>
2025-07-17 18:00:41 -0400

    init project

---
+++ jdbc_api_tutorial/src/main/java/com/example/jdbc_api_tutorial/JdbcApiTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbc_api_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcApiTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcApiTutorialApplication.class, args);
+	}
+
+}
